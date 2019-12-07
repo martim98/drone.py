eca182c347787e965de63a3fbec2eff2992c0968
martim98 <martim.ferreira.almeida@gmail.com>
2019-12-07 22:48:30 +0000

    add prepare.py forgott previously

---
+++ prepare.py
@@ -0,0 +1,11 @@
+#Module to prepare files for the writeFiles
+import math
+
+
+def sortingMaches(list1):
+    list1.sort(key= lambda list1: list1[1])
+    return list1
+
+def reOrganizeDrones(list1):
+ """
+ """
